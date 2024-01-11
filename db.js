608e81dae76ae06a2c04db798e57a9b5bf0829f3
SebastianL2 <sebas16cely@gmail.com>
2024-01-11 01:24:50 -0500

    db chs

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { connect } = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await connect(process.env.MONGODB_URI || "mongodb+srv://sebas16cely:S1lyF1xIibBJXavB@cluster0.czifmw4.mongodb.net/?retryWrites=true&w=majority");
+    await connect(process.env.MONGODB_URI || "mongodb+srv://sebas16cely:68ekljJAgmJucR6D@cluster0.wzd4qpr.mongodb.net/?retryWrites=true&w=majority");
     console.log("Mongodb connected");
   } catch (error) {
     console.error(error);
